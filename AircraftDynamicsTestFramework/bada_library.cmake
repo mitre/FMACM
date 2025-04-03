0c196bb0966db57104e7ea4c89bcb20b921c97e9
klewis <klewis@mitre.org>
2025-04-03 11:13:37 -0400

    remove bada_library.cmake

--- AircraftDynamicsTestFramework/bada_library.cmake
+++
@@ -1,6 +0,0 @@
-# This file is optional.
-
-# A bada implementation library is needed to fully run FMACM, but
-# to compile. 
-set(BADA_LIBRARY bada)
-message("mitre internal bada library included in build")

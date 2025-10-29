9d305ed422df9ccf7304de86fa7bb7a7c6410d7e
Stuart Bowman <sbowman@mitre.org>
2025-10-29 12:59:20 -0400

    bugfix: one more temp change for #31

--- AircraftDynamicsTestFramework/framework.cmake
+++ AircraftDynamicsTestFramework/framework.cmake
@@ -1,20 +1,21 @@
 add_subdirectory(${FRAMEWORK_DIR})
 
 if(NOT ${BUILD_LIBARIES_ONLY})
-    CPMAddPackage(
-        NAME cppmanifest_fmacm
-        GIT_REPOSITORY https://mustache.mitre.org/scm/idea/cppmanifest.git
-        GIT_TAG v1.7
-        DOWNLOAD_ONLY TRUE
-    )
-    if(cppmanifest_fmacm_ADDED AND IS_READABLE "${cppmanifest_fmacm_SOURCE_DIR}")
-        include(${cppmanifest_fmacm_SOURCE_DIR}/cppmanifest_configure.cmake)
-        set(PATH_FOR_CPPMANIFEST "${CMAKE_CURRENT_BINARY_DIR}/include/cppmanifest")
-        cppmanifest_configure(${PATH_FOR_CPPMANIFEST})
-        set(cppmanifest_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/include")
-    else()
-        message(FATAL_ERROR "Unable to configure cppmanifest header files: ${cppmanifest_fmacm_SOURCE_DIR}")
-    endif()
+    # CPMAddPackage(
+    #     NAME cppmanifest_fmacm
+    #     GIT_REPOSITORY https://mustache.mitre.org/scm/idea/cppmanifest.git
+    #     GIT_TAG v1.7
+    #     DOWNLOAD_ONLY TRUE
+    # )
+    # if(cppmanifest_fmacm_ADDED AND IS_READABLE "${cppmanifest_fmacm_SOURCE_DIR}")
+    #     include(${cppmanifest_fmacm_SOURCE_DIR}/cppmanifest_configure.cmake)
+    #     set(PATH_FOR_CPPMANIFEST "${CMAKE_CURRENT_BINARY_DIR}/include/cppmanifest")
+    #     cppmanifest_configure(${PATH_FOR_CPPMANIFEST})
+    #     set(cppmanifest_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/include")
+    # else()
+    #     message(FATAL_ERROR "Unable to configure cppmanifest header files: ${cppmanifest_fmacm_SOURCE_DIR}")
+    # endif()
+    set(cppmanifest_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/include")
 
     SET(FMACM_MAIN_SRC ${FRAMEWORK_DIR}/fmacm.cpp)
 
